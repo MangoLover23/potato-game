956559eb8bb5b52fb9ac084def5e27ef38b1f3d8
Mo Rungjang <112864184+MangoLover23@users.noreply.github.com>
2023-02-13 12:03:28 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,13 +1,34 @@
-let stel = 0
 input.onGesture(Gesture.Shake, function () {
-    stel = 2
+    if (toestand == 1) {
+        stel = 2
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        radio.sendNumber(aardappel)
+    }
 })
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "einde") {
+        if (toestand == 2) {
+            toestand = 4
+            basic.showIcon(IconNames.Happy)
+        }
+    }
+})
+let stel = 0
+let aardappel = 0
+let toestand = 0
+radio.setGroup(1)
+basic.showIcon(IconNames.Target)
+toestand = 1
+aardappel = randint(30, 60)
+let timerEindtijd = input.runningTime() + 300
 basic.forever(function () {
-    basic.showLeds(`
-        # # . . .
-        . # # . .
-        . # . . .
-        . # . . .
-        . . . . .
-        `)
+    if (toestand == 1) {
+    	
+    }
 })
