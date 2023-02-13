1374f6d37c746597d6ed2f87dadef79ccade91de
Mo Rungjang <112864184+MangoLover23@users.noreply.github.com>
2023-02-13 11:46:49 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,3 +1,13 @@
+let stel = 0
+input.onGesture(Gesture.Shake, function () {
+    stel = 2
+})
 basic.forever(function () {
-	
+    basic.showLeds(`
+        # # . . .
+        . # # . .
+        . # . . .
+        . # . . .
+        . . . . .
+        `)
 })
